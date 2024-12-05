320470f7f53f45aed36e19ed0c6d995b8f6c8a1e
Alexamakans <fougner.alexander@gmail.com>
2024-12-05 23:32:33 +0100

    move stuff around

--- 2024/src/1.cpp
+++ 2024/src/1.cpp
@@ -2,13 +2,20 @@
 #include <sstream>
 #include <string>
 
-int part_one(const std::string& input) {
+int part_one(const std::string &input) {
+  // Pair up the smallest number in the left list with the smallest number in the right list,
+  // second smallest with the second smallest, etc.
+  //
+  // Output the sum of the difference of each pair.
+
+  std::istringstream lines(input);
+  std::string line;
+  while (std::getline(lines, line)) {
+  }
   return 0;
 }
 
-int part_two(const std::string& input) {
-  return 0;
-}
+int part_two(const std::string &input) { return 0; }
 
 int main() {
   std::ostringstream buffer;
@@ -18,7 +25,7 @@ int main() {
   std::cout << " --- PART 1 LOGS ---" << std::endl;
   int part_one_result = part_one(input);
   std::cout << std::endl << std::endl;
-  
+
   std::cout << " --- PART 2 LOGS ---" << std::endl;
   int part_two_result = part_two(input);
   std::cout << std::endl << std::endl;
---
+++ README.md
@@ -0,0 +1,3 @@
+Using Advent of Code to relearn [cpp](https://en.wikipedia.org/wiki/C%2B%2B).
+
+Uses [entr](https://github.com/eradman/entr) to watch run commands when files change.
