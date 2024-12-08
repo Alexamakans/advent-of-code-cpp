0e06c0c37c5f3ca5761ed2dd63f7c3a7e486aaa3
Alexamakans <fougner.alexander@gmail.com>
2024-12-09 00:08:48 +0100

    2024 - 6 - 2

--- 2024/src/6.cpp
+++ 2024/src/6.cpp
@@ -2,6 +2,7 @@
 #include <format>
 #include <iostream>
 #include <sstream>
+#include <unordered_set>
 
 #include "util.hpp"
 
@@ -11,36 +12,47 @@ using std::unexpected, std::expected;
 
 typedef int AnswerType;
 
-auto part_one(const string &input) -> expected<AnswerType, string> {
-  AnswerType result = 0;
-  struct Tile {
-    char c;
-    bool visited;
-  };
-  std::vector<std::vector<Tile>> grid;
+constexpr int RIGHT = 0;
+constexpr int DOWN = 1;
+constexpr int LEFT = 2;
+constexpr int UP = 3;
 
-  {
-    std::istringstream lines(input);
-    string line;
-    while (std::getline(lines, line)) {
-      grid.push_back({});
-      for (const char c : line) {
-        grid.back().push_back({.c = c, .visited = false});
-      }
-    }
-  }
+struct Point {
+  int x, y;
 
-  int sx, sy;
-  constexpr int RIGHT = 0;
-  constexpr int DOWN = 1;
-  constexpr int LEFT = 2;
-  constexpr int UP = 3;
+  bool operator==(const Point &rhs) const { return x == rhs.x && y == rhs.y; }
+};
+
+struct Step {
   int direction;
+  Point from;
 
-  for (const auto &[y, line] : std::ranges::views::enumerate(grid)) {
-    for (const auto &[x, tile] : std::ranges::views::enumerate(line)) {
-      if (tile.c == '^' || tile.c == '>' || tile.c == 'v' || tile.c == '<') {
-        switch (tile.c) {
+  bool operator==(const Step &rhs) const {
+    return direction == rhs.direction && from == rhs.from;
+  }
+};
+
+struct point_hash {
+  std::size_t operator()(const Point &v) const {
+    return v.x * 10 + v.y * 1000000;
+  }
+};
+
+struct step_hash {
+  std::size_t operator()(const Step &v) const {
+    return v.direction + point_hash{}(v.from);
+    ;
+  }
+};
+
+// x,y,direction
+std::tuple<int, int, int>
+get_starting_position(const std::vector<std::vector<char>> &grid) {
+  for (const auto &[ty, line] : std::ranges::views::enumerate(grid)) {
+    for (const auto &[tx, c] : std::ranges::views::enumerate(line)) {
+      if (c == '^' || c == '>' || c == 'v' || c == '<') {
+        int direction;
+        switch (c) {
         case '>':
           direction = RIGHT;
           break;
@@ -54,18 +66,25 @@ auto part_one(const string &input) -> expected<AnswerType, string> {
           direction = UP;
           break;
         }
-        sx = x;
-        sy = y;
-        tile.visited = true;
-        ++result;
-        goto found_start_position_p1;
+        return {tx, ty, direction};
       }
     }
   }
-found_start_position_p1:
+  throw "oh no";
+}
 
-  int x = sx;
-  int y = sy;
+// Returns the steps taken, and if it resulted in a loop or not.
+std::tuple<std::unordered_set<Step, step_hash>, bool> walk(
+    const std::vector<std::vector<char>> &grid,
+    std::optional<std::tuple<int, int, int>> starting_position = std::nullopt) {
+  if (!starting_position) {
+    starting_position = get_starting_position(grid);
+  }
+  auto [x, y, direction] = starting_position.value();
+  std::unordered_set<Step, step_hash> steps;
+  steps.insert({direction, {x, y}});
+  int width = grid.at(0).size();
+  int height = grid.size();
   while (true) {
     int next_x = x;
     int next_y = y;
@@ -84,45 +103,88 @@ found_start_position_p1:
       break;
     }
 
-    if (next_x >= 0 && next_x < grid.at(0).size() && next_y >= 0 &&
-        next_y < grid.size()) {
-      auto &tile = grid.at(next_y).at(next_x);
-      if (tile.c == '#') {
+    if (next_x >= 0 && next_x < width && next_y >= 0 && next_y < height) {
+      char c = grid.at(next_y).at(next_x);
+      Step step{direction, {next_x, next_y}};
+      bool duplicate = steps.contains(step);
+      if (duplicate) {
+        return std::make_tuple(steps, true);
+      }
+      if (c == '#') {
         direction = (direction + 1) % 4;
       } else {
         x = next_x;
         y = next_y;
-        if (!tile.visited) {
-          ++result;
-          tile.visited = true;
-        }
+        steps.insert(step);
       }
     } else {
       break;
     }
   }
 
+  return std::make_tuple(steps, false);
+}
+
+auto part_one(const string &input) -> expected<AnswerType, string> {
+  AnswerType result = 0;
+  std::vector<std::vector<char>> grid;
+
+  {
+    std::istringstream lines(input);
+    string line;
+    while (std::getline(lines, line)) {
+      grid.emplace_back(line.begin(), line.end());
+    }
+  }
+
+  const auto &[steps, loop] = walk(grid);
+  std::unordered_set<Point, point_hash> unique_points;
+  for (const auto &step : steps) {
+    unique_points.insert(step.from);
+  }
+  result = unique_points.size();
+
   return result;
 }
 
 auto part_two(const string &input) -> expected<AnswerType, string> {
-  // We probably need to keep track of the direction that we leaved a tile with.
-  //
-  // If we can place an obstruction to replicate the direction we leaved the tile with we
-  // are likely to get caught in a loop? The obstruction could interfer with the path back though.
-  //
-  // Idk
   AnswerType result = 0;
+  std::vector<std::vector<char>> grid;
 
-  std::istringstream lines(input);
-  string line;
-  while (std::getline(lines, line)) {
-    // Do stuff
+  {
+    std::istringstream lines(input);
+    string line;
+    while (std::getline(lines, line)) {
+      grid.emplace_back(line.begin(), line.end());
+    }
   }
 
-  if (false) {
-    return unexpected("false is true?!");
+  const auto &[steps, loop] = walk(grid);
+  std::unordered_set<Point, point_hash> unique_points;
+  for (const auto &step : steps) {
+    unique_points.insert(step.from);
   }
+
+  const auto starting_point = get_starting_position(grid);
+
+  std::optional<Point> previous;
+  for (const auto &p : unique_points) {
+    if (previous) {
+      auto &c = grid.at(previous.value().y).at(previous.value().x);
+      c = '.';
+    }
+    auto &c = grid.at(p.y).at(p.x);
+    if (c == '^' || c == '>' || c == 'v' || c == '<') {
+      continue;
+    }
+    previous = p;
+    c = '#';
+    const auto &[_, loop] = walk(grid, starting_point);
+    if (loop) {
+      ++result;
+    }
+  }
+
   return result;
 }
 
